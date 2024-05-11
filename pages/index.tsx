c6ebea87ad2a39a8aa4a9993d631fc4464ee52ea
Augustine Nani <augustine.nani@hubtel.com>
2024-05-11 11:31:42 +0000

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main>
         <div>
-          {/* <!-- Spinner Start --> */}
+          {/* <!-- Spinner Start s --> */}
           {/* <div
             id="spinner"
             className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
