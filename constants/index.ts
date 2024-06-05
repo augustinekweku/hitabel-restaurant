484c34f4699270fe5d97c1166c5cfa3181f12815
Augustine Nani <augustine.nani@hubtel.com>
2024-06-05 13:39:42 +0000

    update constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,32 +1,29 @@
 export const BREAKFAST_MENU = [
-    {
-        id: 1,
-        title: "Pancake",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        price: 10,
-        image: "assets/img/menu-1.jpg",
-    },
-    ];
-
+  {
+    id: 1,
+    title: "Pancake",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 10,
+    image: "assets/img/menu-1.jpg",
+  },
+];
 
 export const LUNCH_MENU = [
-    {
-        id: 1,
-        title: "Pancake",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        price: 10,
-        image: "assets/img/menu-1.jpg",
-    },
-    ];
-
-
+  {
+    id: 1,
+    title: "Pancake",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 10,
+    image: "assets/img/menu-1.jpg",
+  },
+];
 
 export const DINNER_MENU = [
-    {
-        id: 1,
-        title: "Pancake",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        price: 10,
-        image: "assets/img/menu-1.jpg",
-    },
-    ];
+  {
+    id: 1,
+    title: "Pancake",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 10,
+    image: "assets/img/menu-1.jpg",
+  },
+];
